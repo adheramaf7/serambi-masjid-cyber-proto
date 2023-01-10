510ec185a067964280d0beb8256b3b0d64a221fb
Adhe Rama F <adheramaf7@gmail.com>
2023-01-11 05:47:57 +0700

    config browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Heading } from '@chakra-ui/react';
 import Login from './Pages/Login';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
-  return <Login />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
